5a39879c958f1d5ff0c6b70b1e5001a22b4865b6
Mark Chavez <markchav3z@gmail.com>
2016-03-12 16:15:15 +0800

    move to new method (wip)

--- app/controllers/cloudhealth_controller.rb
+++ app/controllers/cloudhealth_controller.rb
@@ -4,23 +4,29 @@ class CloudhealthController < ApplicationController
   def index
     render text: "no sprint specified" and return unless params[:sprint]
     
-    @issues = @jira_client.Issue.jql("PROJECT = REL", max_results: 500_000)
+    @issues = fetch_data
+  end
 
-    @issues = @issues.select do |issue|
+  def fetch_data
+    issues = @jira_client.Issue.jql("PROJECT = REL", max_results: 500_000)
+
+    issues = issues.select do |issue|
       sprint = issue.customfield_10007
       sprint.first =~ /name=#{params[:sprint]},/ rescue false
     end
   
-    @issues = @issues.group_by do |issue|
+    issues = issues.group_by do |issue|
       issue.assignee.try(:name)
     end.select { |k, v| k.present? }
 
-    @issues = @issues.map do |username, issues|
+    issues = issues.map do |username, issues|
       story_points = issues.sum do |issue|
         issue.customfield_10004 || 0
       end
 
       { name: username, total: story_points }
     end.sort_by { |hash| hash[:total] }.reverse
+
+    issues
   end
 end
